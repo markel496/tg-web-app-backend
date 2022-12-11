1aaac2c8f68bdd92c5d4f999f329115d8185cbf7
Ivan <markelov.ivan1996@yandex.ru>
2022-12-11 23:43:27 +0300

    update webAppUrl + some fix

--- api/product.js
+++ api/product.js
@@ -18,4 +18,33 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.post('/web-data', async (req, res) => {
+  const { queryId, products, totalPrice } = req.body
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Успешная покупка',
+      input_message_content: {
+        message_text: `Поздравляю с покупкой! Вы приобрели ${products
+          .map((product) => product.title)
+          .join(', ')} на сумму ${totalPrice}`
+      }
+    })
+    return res.status(200).json({ success: true })
+  } catch (e) {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Не удалось приобрести товар',
+      input_message_content: {
+        message_text: 'Не удалось приобрести товар'
+      }
+    })
+    return res.status(500).json({})
+  }
+})
+
+/** Метод answerWebAppQuery на вход ожидает queryId, который является соединяющим звеном для взаимодействия с ботом */
+
 module.exports = router
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const product = require('./api/product')
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '5601866191:AAEY7F3tBgxmnwVV3xz4dcQdKKjVBPyLJ1o'
 
-const webAppUrl = 'https://4000-31-134-188-155.eu.ngrok.io'
+const webAppUrl = 'https://tg-web-app-frontend.vercel.app'
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true })
 
@@ -54,36 +54,7 @@ bot.on('message', async (msg) => {
   }
 })
 
-app.post('/web-data', async (req, res) => {
-  const { queryId, products, totalPrice } = req.body
-  try {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Успешная покупка',
-      input_message_content: {
-        message_text: `Поздравляю с покупкой! Вы приобрели ${products
-          .map((product) => product.title)
-          .join(', ')} на сумму ${totalPrice}`
-      }
-    })
-    return res.status(200).json({ success: true })
-  } catch (e) {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Не удалось приобрести товар',
-      input_message_content: {
-        message_text: 'Не удалось приобрести товар'
-      }
-    })
-    return res.status(500).json({})
-  }
-})
-
-/** Метод answerWebAppQuery на вход ожидает queryId, который является соединяющим звеном для взаимодействия с ботом */
-
-app.use('/api/product', product)
+app.use('/', product)
 
 const PORT = process.env.PORT || 4200
 
