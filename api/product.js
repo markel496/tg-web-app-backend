e2b8556c293cb962dcd1bbad64b63df20804177b
Ivan <markelov.ivan1996@yandex.ru>
2022-12-12 00:10:45 +0300

    update

--- api/product.js
+++ api/product.js
@@ -1,3 +1,4 @@
+const TelegramBot = require('node-telegram-bot-api')
 const express = require('express')
 const router = express.Router()
 
@@ -6,6 +7,52 @@ const router = express.Router()
  *
  * @return product list | empty.
  */
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '5601866191:AAEY7F3tBgxmnwVV3xz4dcQdKKjVBPyLJ1o'
+
+const webAppUrl = 'https://tg-web-app-frontend.vercel.app'
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true })
+
+//Взаимодействие с пользователем
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id
+  const text = msg.text //Вытаскиваю текст из сообщения, которое отправил пользователь
+
+  if (text === '/start') {
+    await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+      reply_markup: {
+        keyboard: [
+          [{ text: 'Заполнить форму', web_app: { url: webAppUrl + '/form' } }]
+        ]
+      }
+    })
+
+    await bot.sendMessage(chatId, 'Заходи на наш сайт', {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Сделать заказ', web_app: { url: webAppUrl } }]
+        ]
+      }
+    })
+  }
+
+  //Если в сообщении прилетело поле web_app_data и поле data у него непутое, то обрабатываю данные
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data) //изначально data в виде строки
+      await bot.sendMessage(chatId, 'Спасибо за обратную связь')
+      await bot.sendMessage(chatId, `Ваша страна: ${data.country}`)
+      await bot.sendMessage(chatId, `Ваша улица: ${data.street}`)
+      setTimeout(async () => {
+        await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате')
+      }, 2000)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+})
+
 router.get('/', async (req, res) => {
   try {
     res.json({
--- index.js
+++ index.js
@@ -1,58 +1,11 @@
-const TelegramBot = require('node-telegram-bot-api')
 const express = require('express')
 const cors = require('cors')
 const product = require('./api/product')
-
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '5601866191:AAEY7F3tBgxmnwVV3xz4dcQdKKjVBPyLJ1o'
-
-const webAppUrl = 'https://tg-web-app-frontend.vercel.app'
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true })
 
 const app = express()
 
 app.use(express.json()) //позволит парсить json
 app.use(cors()) //для кросс-доменных запросов
-
-//Взаимодействие с пользователем
-bot.on('message', async (msg) => {
-  const chatId = msg.chat.id
-  const text = msg.text //Вытаскиваю текст из сообщения, которое отправил пользователь
-
-  if (text === '/start') {
-    await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
-      reply_markup: {
-        keyboard: [
-          [{ text: 'Заполнить форму', web_app: { url: webAppUrl + '/form' } }]
-        ]
-      }
-    })
-
-    await bot.sendMessage(chatId, 'Заходи на наш сайт', {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: 'Сделать заказ', web_app: { url: webAppUrl } }]
-        ]
-      }
-    })
-  }
-
-  //Если в сообщении прилетело поле web_app_data и поле data у него непутое, то обрабатываю данные
-  if (msg?.web_app_data?.data) {
-    try {
-      const data = JSON.parse(msg?.web_app_data?.data) //изначально data в виде строки
-      await bot.sendMessage(chatId, 'Спасибо за обратную связь')
-      await bot.sendMessage(chatId, `Ваша страна: ${data.country}`)
-      await bot.sendMessage(chatId, `Ваша улица: ${data.street}`)
-      setTimeout(async () => {
-        await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате')
-      }, 2000)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-})
 
 app.use('/', product)
 
